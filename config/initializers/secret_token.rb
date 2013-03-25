cff6376a4e237bcf7dbc9b467858f4d06bd09410
Nate Fischer <nate@componentsearch.com>
2013-03-24 22:39:58 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheFishery::Application.config.secret_token = '75cca7557120e9cfe460fcd16a587700fa5a0d7579e59172dda14e163a50c164c2394a3c65d9e1d1dd744c48d1a6d6f801a7ed64dc16148d1f9b9b750681db61'
